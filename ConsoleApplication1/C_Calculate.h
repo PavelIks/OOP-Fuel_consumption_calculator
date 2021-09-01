8c2645fc771a193a8c5ad3fed705a9bbb71a9427
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-01 15:14:43 +0300

    Updated-1.

---
+++ ConsoleApplication1/C_Calculate.h
@@ -0,0 +1,9 @@
+#pragma once
+namespace stv
+{
+	class C_Calculate
+	{
+	public:
+		virtual float calculate() = 0;
+	};
+}
---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "C_Calculate.h"
+#include "C_Consumption.h"
+#include "C_Count.h"
+
+int main()
+{
+    stv::consumption::C_Count cost(100.45, 35.85);
+
+    std::cout << cost.calculate();
+}
